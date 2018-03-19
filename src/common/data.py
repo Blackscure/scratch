eda524a5f2f76c98fa8ba9e66922f1f6c583dbf1
blackscure <wekesabuyahi@gmail.com>
2018-03-19 20:44:46 +0300

    first commit

---
+++ src/common/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title': 'Article one',
+            'body': 'lorem kjuegudeebdkbweivoue hbgdyivgi3 gudieigdi',
+            'author': 'Blacks Cure',
+            'create_date':'04-25-2017'
+        },
+          {
+            'id':2,
+            'title': 'Article Two',
+            'body': 'lorem kjuegudeebdkbweivoue hbgdyivgi3 gudieigdi',
+            'author': 'disiignner',
+            'create_date':'04-25-2017'
+        },
+          {
+            'id':3,
+            'title': 'Article Three',
+            'body': 'lorem kjuegudeebdkbweivoue hbgdyivgi3 gudieigdi',
+            'author': 'Tunchi',
+            'create_date':'04-25-2017'
+        }
+    ]
+    return articles
